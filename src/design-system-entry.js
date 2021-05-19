a7a8f20fd45291dadcde97b4be21e138f4f43e9d
Tom Coleman <tom@thesnail.org>
2021-05-19 15:06:05 +1000

    Add design-system entry

--- src/design-system-entry.js
+++ src/design-system-entry.js
@@ -2,11 +2,8 @@ import { configure } from './storybook';
 
 const globalConfig = require('../design-system/.storybook/preview.js');
 
-const context = require.context(
-  '../design-system/src/components',
-  true,
-  /.*\.stories\.js$/,
-  'lazy'
-);
+const context = require.context('../design-system/src', true, /.*\.stories\.[j|t]sx?$/, 'lazy');
+const storiesJson = require('../stories-json/design-system.stories.json');
+console.log(storiesJson);
 
-configure(context, globalConfig);
+configure(context, storiesJson, globalConfig);
