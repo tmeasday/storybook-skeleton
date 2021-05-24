16dbb59555913e8086ebb10309f9906099d7d9c4
Juho Vepsäläinen <bebraw@gmail.com>
2021-05-24 18:22:04 +0300

    feat: Add webpack-dev-server as a dev option

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const argv = require("webpack-nano/argv");
 const composeConfiguration = require("./compose-configuration");
 
 module.exports = composeConfiguration({
-  target: argv.target,
+  target: process.env.TARGET || argv.target,
   project: process.env.PROJECT || "template",
   builder: process.env.BUILDER || "esbuild",
   compileLazily: process.env.COMPILE_LAZILY === "1",
   profileCpu: process.env.PROFILE_CPU === "1",
   enableSourceMaps: process.env.ENABLE_SOURCE_MAPS === "1",
   enableFsCache: process.env.ENABLE_FS_CACHE === "1",
+  devServer: process.env.DEV_SERVER || "wds",
 });
