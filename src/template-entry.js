4570136dece37b10ca3394f9dab74a658a137a0a
Tom Coleman <tom@thesnail.org>
2021-05-24 17:12:05 +1000

    Three different ways to import template stories

--- src/template-entry.js
+++ src/template-entry.js
@@ -1,6 +1,27 @@
-import { configure } from './storybook';
+import { configure } from "./storybook";
 
-const context = require.context('./template', true, /.*\.stories\.js$/, 'lazy');
-const storiesJson = require('../stories-json/template.stories.json');
+// 1. Standard lazy require context
+const importFn = require.context(
+  "./template",
+  true,
+  /.*\.stories\.jsx$/,
+  "lazy"
+);
 
-configure(context, storiesJson);
+// 2. Using a "shaped" async import
+// const importFn = async (name) => {
+//   const parsedName = name.match(/^\.\/(.*)\.jsx$/)[1];
+//   return import(`./template/${parsedName}.jsx`);
+// };
+
+// 3. Using a static async import
+// const libraries = {
+//   "./Button.stories.jsx": () => import(`./template/Button.stories.jsx`),
+//   "./Header.stories.jsx": () => import(`./template/Header.stories.jsx`),
+//   "./Page.stories.jsx": () => import(`./template/Page.stories.jsx`),
+// };
+// const importFn = (name) => libraries[name]();
+
+const storiesJson = require("../stories-json/template.stories.json");
+
+configure(importFn, storiesJson);
