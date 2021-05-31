7f2cd2d74db0ac501adf1c0d3c24688dbe570f5a
Tom Coleman <tom@thesnail.org>
2021-05-31 12:34:47 +1000

    Ensure we don't bundle `node_modules` in chromatic

--- src/chromatic-entry-import-lazy-require-context.js
+++ src/chromatic-entry-import-lazy-require-context.js
@@ -1,6 +1,31 @@
-export const importFn = require.context(
-  "../chromatic/services/webapp",
-  true,
-  /.*\.stories\.js$/,
-  "lazy"
-);
+const contexts = [
+  require.context(
+    "../chromatic/services/webapp/components",
+    true,
+    /\.stories\.js$/,
+    "lazy"
+  ),
+  require.context(
+    "../chromatic/services/webapp/containers",
+    true,
+    /\.stories\.js$/,
+    "lazy"
+  ),
+  require.context(
+    "../chromatic/services/webapp/layouts",
+    true,
+    /\.stories\.js$/,
+    "lazy"
+  ),
+  require.context(
+    "../chromatic/services/webapp/screens",
+    true,
+    /\.stories\.js$/,
+    "lazy"
+  ),
+];
+
+export const importFn = (name) => {
+  const [_, contextName, path] = name.split("/", 3);
+  return contexts[contextName](`./${path}`);
+};
--- src/chromatic-entry-import-require-context.js
+++ src/chromatic-entry-import-require-context.js
@@ -1,5 +1,27 @@
-export const importFn = require.context(
-  "../chromatic/services/webapp",
-  true,
-  /.*\.stories\.js$/
-);
+const contexts = {
+  components: require.context(
+    "../chromatic/services/webapp/components",
+    true,
+    /\.stories\.js$/
+  ),
+  containers: require.context(
+    "../chromatic/services/webapp/containers",
+    true,
+    /\.stories\.js$/
+  ),
+  layouts: require.context(
+    "../chromatic/services/webapp/layouts",
+    true,
+    /\.stories\.js$/
+  ),
+  screens: require.context(
+    "../chromatic/services/webapp/screens",
+    true,
+    /\.stories\.js$/
+  ),
+};
+
+export const importFn = (name) => {
+  const [_, contextName, path] = name.split("/", 3);
+  return contexts[contextName](`./${path}`);
+};
