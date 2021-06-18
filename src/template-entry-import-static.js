79147e73763078483ef4e81ce6a53b8f29c9ccbc
Tom Coleman <tom@thesnail.org>
2021-06-18 20:19:09 +1000

    Remove old entries

--- src/template-entry-import-static.js
+++
@@ -1,6 +0,0 @@
-const libraries = {
-  "./Button.stories.jsx": () => import(`./template/Button.stories.jsx`),
-  "./Header.stories.jsx": () => import(`./template/Header.stories.jsx`),
-  "./Page.stories.jsx": () => import(`./template/Page.stories.jsx`),
-};
-export const importFn = (name) => libraries[name]();
